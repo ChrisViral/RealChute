5bea3e2ea6089e78b90fdc9e6ac519b4b1abd882
Christophe Savard <christophe_savard@hotmail.ca>
2017-05-17 05:04:24 -0400

    Add UI project
    Also ignore Unity folder

---
+++ RealChuteUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+/* RealChute was made by Christophe Savard (stupid_chris). You are free to copy, fork, and modify RealChute as you see
+ * fit. However, redistribution is only permitted for unmodified versions of RealChute, and under attribution clause.
+ * If you want to distribute a modified version of RealChute, be it code, textures, configs, or any other asset and
+ * piece of work, you must get my explicit permission on the matter through a private channel, and must also distribute
+ * it through the attribution clause, and must make it clear to anyone using your modification of my work that they
+ * must report any problem related to this usage to you, and not to me. This clause expires if I happen to be
+ * inactive (no connection) for a period of 90 days on the official KSP forums. In that case, the license reverts
+ * back to CC-BY-NC-SA 4.0 INTL. */
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RealChuteUI")]
+[assembly: AssemblyDescription("RealChute Unity UI Implementation")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RealChuteUI")]
+[assembly: AssemblyCopyright("Copyright © Christophe Savard (stupid_chris) 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("94ec9b40-a1fd-4d5a-b82a-ed224dfbf0d7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
